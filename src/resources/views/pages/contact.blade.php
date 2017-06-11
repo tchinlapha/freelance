23ff3213c1355dcd5dd829fc8416b130fb2a06d0
tchinlapha <tchinlapha@gmail.com>
2017-06-11 22:05:33 +0700

    fix text contact

--- src/resources/views/pages/contact.blade.php
+++ src/resources/views/pages/contact.blade.php
@@ -42,25 +42,25 @@
 		<div id="sidebar" class="col-md-4">
 			<div class="sidebar-item widget">
 
-				<h6 class="widget-title">ที่อยู่</h6>
+				<h6 class="contact-title">ที่อยู่</h6>
 				<ul>
 					<li>
 						เสรีชัย อินทีเรียร์ ดีไซน์ จำกัด 1647 อ่อนนุช แขวง สวนหลวง เขต สวนหลวง กรุงเทพมหานคร 10250
 					</li>
 				</ul>
-				<h6 class="widget-title">อีเมลล์บริษัท</h6>
+				<h6 class="contact-title">อีเมลล์บริษัท</h6>
 				<ul>
 					<li>
 						sereechai_int@hotmail.com
 					</li>
 				</ul>
-				<h6 class="widget-title">เบอรโทรศัพท์</h6>
+				<h6 class="contact-title">เบอรโทรศัพท์</h6>
 				<ul>
 					<li>
 						02-742-0781
 					</li>
 				</ul>
-				<h6 class="widget-title">เวลาทำการ</h6>
+				<h6 class="contact-title">เวลาทำการ</h6>
 				<ul>
 					<li>
 						จันทร์ - อาทิตย์ เวลา 09.00 - 17.00 น.
@@ -69,7 +69,7 @@
 			</div>
 
 			<div class="sidebar-item widget pyre_social_media-feat">
-				<h5 class="widget-title">ติดตามเรา</h5>
+				<h5 class="contact-title">ติดตามเรา</h5>
 				<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F%25E0%25B9%2580%25E0%25B8%25AA%25E0%25B8%25A3%25E0%25B8%25B5%25E0%25B8%258A%25E0%25B8%25B1%25E0%25B8%25A2-%25E0%25B8%25AD%25E0%25B8%25B4%25E0%25B8%2599%25E0%25B8%2597%25E0%25B8%25B5%25E0%25B9%2580%25E0%25B8%25A3%25E0%25B8%25B5%25E0%25B8%25A2%25E0%25B8%25A3%25E0%25B9%258C-%25E0%25B8%2594%25E0%25B8%25B5%25E0%25B9%2584%25E0%25B8%258B%25E0%25B8%2599%25E0%25B9%258C-%25E0%25B8%2588%25E0%25B8%25B3%25E0%25B8%2581%25E0%25B8%25B1%25E0%25B8%2594-358191721245279&tabs&&height=240&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
 				 width="100%" height="240" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 
