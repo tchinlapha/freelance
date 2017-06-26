99b2ddd38e073bd5aab64a4e7fa5fc100b418961
tchinlapha <tchinlapha@gmail.com>
2017-06-26 21:30:26 +0700

    SQL for slide

--- sereechai.sql
+++ sereechai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2017 at 02:19 PM
+-- Generation Time: Jun 26, 2017 at 04:29 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -40,7 +40,7 @@ CREATE TABLE `about` (
 --
 
 INSERT INTO `about` (`id`, `about`, `created_at`, `updated_at`) VALUES
-(1, '<h2 style=\"text-align: center;\">What is Lorem Ipsum?</h2>\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n<p>&nbsp;</p>\n<h2 style=\"text-align: center;\">Why do we use it?</h2>\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\n<p>&nbsp;</p>\n<h2 style=\"text-align: center;\">Where does it come from?</h2>\n<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.</p>', '2017-06-18 08:05:23', '2017-06-25 10:22:19');
+(1, '<h2 style=\"text-align: center;\">What is Lorem Ipsum?</h2>\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n<p>&nbsp;</p>\n<h2 style=\"text-align: center;\">Why do we use it?</h2>\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\n<p>&nbsp;</p>\n<h2 style=\"text-align: center;\">Where does it come from?</h2>\n<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.</p>', '2017-06-18 08:05:23', '2017-06-25 16:11:13');
 
 -- --------------------------------------------------------
 
@@ -102,7 +102,7 @@ CREATE TABLE `config` (
 --
 
 INSERT INTO `config` (`id`, `address`, `email`, `phone`, `work_time`, `facebook`, `map`, `created_at`, `updated_at`) VALUES
-(1, 'เสรีชัย อินทีเรียร์ ดีไซน์ จำกัด 1647 อ่อนนุช แขวง สวนหลวง เขต สวนหลวง กรุงเทพมหานคร 10250', 'sereechai_int@hotmail.com', '02-742-0781', 'จันทร์ - อาทิตย์ เวลา 09.00 - 17.00 น.', 'https://www.facebook.com/เสรีชัย-อินทีเรียร์-ดีไซน์-จำกัด-358191721245279', '<iframe src=\"https://her.is/2r7b2FV\" frameborder=\"0\" style=\"border:0;min-height: 500px;\"></iframe>', '2017-06-20 15:16:05', '2017-06-20 15:39:45');
+(1, 'เสรีชัย อินทีเรียร์ ดีไซน์ จำกัด 1647 อ่อนนุช แขวง สวนหลวง เขต สวนหลวง กรุงเทพมหานคร 10250', 'sereechai_int@hotmail.com', '02-742-0781', 'จันทร์ - อาทิตย์ เวลา 09.00 - 17.00 น.', 'https://www.facebook.com/เสรีชัย-อินทีเรียร์-ดีไซน์-จำกัด-358191721245279', 'https://her.is/2r7b2FV', '2017-06-20 15:16:05', '2017-06-25 13:06:15');
 
 -- --------------------------------------------------------
 
@@ -126,7 +126,21 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `name`, `email`, `phone`, `message`, `is_read`, `created_at`, `updated_at`) VALUES
-(1, 'test1 ', 'test1@mail.com', '09877645452', 'hello world!', 0, '2017-06-24 12:47:56', '2017-06-24 12:47:56');
+(1, 'test1 ', 'test1@mail.com', '09877645452', 'hello world!', 0, '2017-06-24 12:47:56', '2017-06-24 12:47:56'),
+(2, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'messageee', 0, '2017-06-25 14:24:23', '2017-06-25 14:24:23'),
+(3, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'messageee', 0, '2017-06-25 14:24:34', '2017-06-25 14:24:34'),
+(4, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'message', 0, '2017-06-25 14:29:37', '2017-06-25 14:29:37'),
+(5, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'messageeeeeeeeeeeee', 0, '2017-06-25 14:31:20', '2017-06-25 14:31:20'),
+(6, NULL, NULL, NULL, NULL, 0, '2017-06-25 15:46:02', '2017-06-25 15:46:02'),
+(7, NULL, NULL, NULL, NULL, 0, '2017-06-25 15:46:18', '2017-06-25 15:46:18'),
+(8, NULL, NULL, NULL, NULL, 0, '2017-06-25 15:47:46', '2017-06-25 15:47:46'),
+(9, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', NULL, 0, '2017-06-25 15:54:54', '2017-06-25 15:54:54'),
+(10, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'aaaaaaa', 0, '2017-06-25 15:56:32', '2017-06-25 15:56:32'),
+(11, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'asdasd', 0, '2017-06-25 15:57:43', '2017-06-25 15:57:43'),
+(12, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'dsadasd', 0, '2017-06-25 15:59:11', '2017-06-25 15:59:11'),
+(13, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'asfasf', 0, '2017-06-25 16:00:07', '2017-06-25 16:00:07'),
+(14, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'asfasf', 0, '2017-06-25 16:00:48', '2017-06-25 16:00:48'),
+(15, 'Thanin Chinlapha', 'tchinlapha@gmail.com', '997084982', 'asf', 0, '2017-06-25 16:01:06', '2017-06-25 16:01:06');
 
 -- --------------------------------------------------------
 
@@ -216,10 +230,20 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `slide` (
   `id` int(11) NOT NULL,
+  `text` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `slide`
+--
+
+INSERT INTO `slide` (`id`, `text`, `name`, `path`, `created_at`, `updated_at`) VALUES
+(1, 'test 1', '1.jpg', '/assets/images/slider/1.jpg', '2017-06-26 13:26:41', '2017-06-26 13:26:41'),
+(2, 'test 2', '1_1.jpg', '/assets/images/slider/1_1.jpg', '2017-06-26 13:26:41', '2017-06-26 13:26:41');
 
 -- --------------------------------------------------------
 
@@ -242,7 +266,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ADMIN', 'admin@mail.com', '$2y$10$IB1ybc/1PuPnL1IwXqgCFOUwdj4HT6BDH/w1yOo8lMlJgxks3sRpa', NULL, '2017-06-15 06:18:23', '2017-06-15 06:18:23');
+(1, 'ADMIN', 'admin@mail.com', '$2y$10$IB1ybc/1PuPnL1IwXqgCFOUwdj4HT6BDH/w1yOo8lMlJgxks3sRpa', 'z8EjRAtrj75PTKPcNrdi6RCBelrAIz1K4iq7EbNvOcRyqN9bChE4PIqo9QJC', '2017-06-15 06:18:23', '2017-06-15 06:18:23');
 
 --
 -- Indexes for dumped tables
@@ -332,17 +356,17 @@ ALTER TABLE `config`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `image_album`
 --
 ALTER TABLE `image_album`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
@@ -352,7 +376,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `slide`
 --
 ALTER TABLE `slide`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
