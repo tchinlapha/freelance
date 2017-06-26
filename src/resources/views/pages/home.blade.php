30909a637a377515a564727a1fc6c604e46faf0b
jitraphorn <jitraphorn1@gmail.com>
2017-06-26 22:14:52 +0700

    home

--- src/resources/views/pages/home.blade.php
+++ src/resources/views/pages/home.blade.php
@@ -28,14 +28,56 @@
 </head>
 
 <body>
-	@include('./layouts.header')
+	<header class="animated slideInDown fix-when-mobile" style="position: fixed;z-index: 99999;">
+    <div class="menu-mobile" style="padding:10px;">
+        <div class="pull-left">
+            <img src="{{ asset('assets/images/SEREECHAILOGOV2-01.png') }}" alt="Renovation" style="height:50px;" />
+            <span style="color:#fff;font-size: 16px;">เสรีชัย อินทีเรียร์ ดีไซน์ จำกัด</span>
+        </div>
+        <div class="pull-right">
+            <a href="javascript:void(0)" onclick="toggleSidebar()"><i class="fa fa-bars fa-3x" aria-hidden="true" style="line-height:50px;color:#fff;margin-right:5px;"></i></a>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="menu-side-menu animated">
+        <ul>
+            <li>
+                <a data-ripple href="/" class="{{ $page == "home" ? "active " : "" }}"> <i class="fa fa-home"></i> หน้าแรก</a>
+            </li>
+            <li>
+                <a data-ripple href="/service" class="{{ $page == "service" ? "active " : "" }}"><i class="fa fa-info-circle"></i> เกี่ยวกับบริษัท</a>
+            </li>
+            <li>
+                <a data-ripple href="/portfolio" class="{{ $page == "portfolio" ? "active " : "" }}"><i class="fa fa-picture-o"></i> ผลงานของเรา</a>
+            </li>
+            <li>
+                <a data-ripple href="/contact" class="{{ $page == "contact" ? "active " : "" }}"><i class="fa fa-handshake-o"></i> ติดต่อเรา</a>
+            </li>
+        </ul>
+    </div>
+    <nav>
+        <div class="width-container">
+           <div style="background: #fff;width: 125px;position: absolute;left: 45%;border-radius: 0 0 10px 10px;box-shadow:0px 0px 10px 1px #252536;">
+                <div class="pull-left">
+                    <a href="/" title="Renovation" rel="home">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Renovation" class="img-responsive" />
+                    </a>
+                </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </nav>
+</header>
 	<div id="carousel-example-generic" data-ride="carousel" class="carousel slide carousel-fade">
 	 	<div class="carousel-inner" role="listbox" style="width: 100%;height: 100vh;">
 	 		@foreach($slide as $key => $x)
 	 		<div class="item item-bg {{ $key == 0 ? 'active'  : '' }}" style="background-image:url({{$x->path}})">
 	 			<div class="carousel-caption">
 	 				<h1 data-animation="animated fadeInUp">{{$x->text}}</h1>
+	 				<a href=""><button type="button" class="btn btn-primary btn-lg active">เข้าสู่เว็บไซท์</button></a>
 	 			</div>
+	 			
 	 		</div>
 	 		@endforeach
 
@@ -47,7 +89,9 @@
 	 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 	 			<span class="sr-only">Next</span>
 	 		</a>
+
 	 	</div>
+
 	</div>
 <!-- /.carousel -->
 
